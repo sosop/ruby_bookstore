4b7690f1ecb65325fd13597dc5e7fb36cd4eff30
houxiaolong <houxiaolong@cd.tuan800.com>
2013-07-10 17:29:27 +0800

    in_out2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        sign_in @user
         format.html { redirect_to front_path,notice: '注册成功' }
         format.json { render json: @user, status: :created, location: @user }
       end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,10 @@
 module SessionsHelper
   def sign_in(user)
     #cookies.permanent[:remember_token] = user.remember_token
-    cookies[:remember_token] = user.remember_token
+    #cookies[:remember_token] = user.remember_token
+    cookies[:remember_token] = { value: user.remember_token,
+                                 expires: 30.minutes.from_now.utc }
+
     self.current_user = user
   end
 
